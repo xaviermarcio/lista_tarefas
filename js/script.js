d3aa491f626d0f42ef37d9bc6f0b0c4b63f36f8c
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-08-30 12:53:54 -0300

    finalizando validações no JavaScript

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ function addTarefa(){
         ++contador;
         let novoItem = 
         `<div id="${contador}"class="item">
-            <div class="item-icone">
-                <i class="mdi mdi-circle-outline"></i>
+            <div class="item-icone" onclick="marcarTarefa(${contador})">
+                <i id="icone_${contador}"class="mdi mdi-circle-outline"></i>
             </div>
-            <div class="item-nome">
+            <div class="item-nome" onclick="marcarTarefa(${contador})">
                 ${valorInput}
             </div>
             <div class="item-botao">
@@ -29,12 +29,34 @@ function addTarefa(){
         input.focus();
     }
 }
-
+//funcao delelar
 function deletar(id){
     var tarefa = document.getElementById(id);
     tarefa.remove();
     
 }
+function marcarTarefa(id){
+    var item = document.getElementById(id);
+    var classe = item.getAttribute('class');
+
+    if(classe =="item"){
+        item.classList.add('clicado');
+
+        var icone = document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-circle-outline');
+        icone.classList.add('mdi-check-circle');
+
+        item.parentNode.appendChild(item);
+
+    }else{
+        item.classList.remove('clicado');
+
+        var icone = document.getElementById('icone_'+id);
+        icone.classList.remove('mdi-check-circle');
+        icone.classList.add('mdi-circle-outline');
+    }
+}
+
 //Apertar a tecla enter
 input.addEventListener("keyup", function (event){
     if (event.keyCode === 13 || event.which == "Enter"){
