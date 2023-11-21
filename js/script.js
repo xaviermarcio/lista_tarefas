0166ff3c600cdd4e08829758958db8ab145e6d90
Márcio Xavier <43189549+xaviermarcio@users.noreply.github.com>
2023-11-21 14:34:59 -0300

    Atualizando CSS

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 let contador = 0;
-let input = document.getElementById('inputTarefa');
-let btnAdd = document.getElementById('btn-add');
-let main = document.getElementById('areaLista');
+let input = document.getElementById("inputTarefa");
+let btnAdd = document.getElementById("btn-add");
+let main = document.getElementById("areaLista");
 
-function addTarefa(){
-    //pegar o valor digitado no input
-    let valorInput = input.value;
-    //se nçao for vazio, nem nulo, nem indefinido
-    if((valorInput !== "") && (valorInput !== null) && (valorInput!== undefined)){
-
-        ++contador;
-        let novoItem = 
-        `<div id="${contador}"class="item">
+function addTarefa() {
+  //pegar o valor digitado no input
+  let valorInput = input.value;
+  //se não for vazio, nem nulo, nem indefinido
+  if (valorInput !== "" && valorInput !== null && valorInput !== undefined) {
+    ++contador;
+    let novoItem = `<div id="${contador}"class="item">
             <div class="item-icone" onclick="marcarTarefa(${contador})">
                 <i id="icone_${contador}"class="mdi mdi-circle-outline"></i>
             </div>
@@ -22,45 +20,43 @@ function addTarefa(){
                 <button class="delete"><i class="mdi mdi-delete" onclick="deletar(${contador})">Deletar</i></button>
             </div>
         </div>`;
-        //adicionar novo item na main
-        main.innerHTML += novoItem;
-        //zerar campos
-        input.value = "";
-        input.focus();
-    }
+    //adicionar novo item na main
+    main.innerHTML += novoItem;
+    //zerar campos
+    input.value = "";
+    input.focus();
+  }
 }
 //funcao delelar
-function deletar(id){
-    var tarefa = document.getElementById(id);
-    tarefa.remove();
-    
+function deletar(id) {
+  var tarefa = document.getElementById(id);
+  tarefa.remove();
 }
-function marcarTarefa(id){
-    var item = document.getElementById(id);
-    var classe = item.getAttribute('class');
+function marcarTarefa(id) {
+  var item = document.getElementById(id);
+  var classe = item.getAttribute("class");
 
-    if(classe =="item"){
-        item.classList.add('clicado');
+  if (classe == "item") {
+    item.classList.add("clicado");
 
-        var icone = document.getElementById('icone_'+id);
-        icone.classList.remove('mdi-circle-outline');
-        icone.classList.add('mdi-check-circle');
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-circle-outline");
+    icone.classList.add("mdi-check-circle");
 
-        item.parentNode.appendChild(item);
+    item.parentNode.appendChild(item);
+  } else {
+    item.classList.remove("clicado");
 
-    }else{
-        item.classList.remove('clicado');
-
-        var icone = document.getElementById('icone_'+id);
-        icone.classList.remove('mdi-check-circle');
-        icone.classList.add('mdi-circle-outline');
-    }
+    var icone = document.getElementById("icone_" + id);
+    icone.classList.remove("mdi-check-circle");
+    icone.classList.add("mdi-circle-outline");
+  }
 }
 
 //Apertar a tecla enter
-input.addEventListener("keyup", function (event){
-    if (event.keyCode === 13 || event.which == "Enter"){
-        event.preventDefault();    
-        btnAdd.click();
-    }    
-})
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13 || event.which == "Enter") {
+    event.preventDefault();
+    btnAdd.click();
+  }
+});
